fc41c003d194674715d0d68f65d969e23188786b
WeiProduct <1597498880weiproduct@gmail.com>
2025-07-13 09:23:51 -0400

    Initial Commit

---
+++ AITouchGrassTests/AITouchGrassTests.swift
@@ -0,0 +1,17 @@
+//
+//  AITouchGrassTests.swift
+//  AITouchGrassTests
+//
+//  Created by weifu on 7/13/25.
+//
+
+import Testing
+@testable import AITouchGrass
+
+struct AITouchGrassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
